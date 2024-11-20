5f3ddb8f41d3048f9b24a98ce8b8bb3bea486b47
philbjern <filipbiernat@gmail.com>
2024-11-20 10:48:11 +0100

    add final salon project code

--- salon/salon.sh
+++ salon/salon.sh
@@ -28,17 +28,36 @@ MAIN_MENU() {
 }
 
 SERVICE_MENU() {
-	SERVICE_ID_SELECTED=$1
-	echo -e "\nWhat's your phone number?"
-	read CUSTOMER_PHONE
-	PHONE_RESULT=$($PSQL "SELECT customer_id, name FROM customers WHERE phone='$CUSTOMER_PHONE'")
-	if [[ -z $PHONE_RESULT ]]
+	if [[ -z $1 ]]
 	then
-		echo -e "\nI don't have a record for that phone number, what's your name?"
-		read CUSTOMER_NAME
-		CUSTOMER_INSERT_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+		MAIN_MENU "I could not find that service. What would you like today?"
+	else
+		SERVICE_NAME=$1
+		echo -e "\nWhat's your phone number?"
+		read CUSTOMER_PHONE
+		CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+		if [[ -z $CUSTOMER_ID ]]
+		then
+			echo -e "\nI don't have a record for that phone number, what's your name?"
+			read CUSTOMER_NAME
+			if [[ -z $CUSTOMER_NAME ]]
+			then
+				MAIN_MENU "You need to input valid name"
+			else
+				CUSTOMER_INSERT_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+				CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+			fi
+		fi
+		
+		CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE customer_id='$CUSTOMER_ID'")
+		CUSTOMER_NAME_FORMATTED=$(echo $CUSTOMER_NAME | sed -r 's/^ *//')
+		SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE name='$SERVICE_NAME'")
+		echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME_FORMATTED?"
+		read SERVICE_TIME
+		APPOINTENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID', '$SERVICE_TIME')")
+		echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME_FORMATTED.\n"
+	
 	fi
-
 
 }
 
